4607a83f32c22cfb7e0f21565e0a80e675dda069
Maximilian Alba <albam476@gmail.com>
2022-05-30 16:14:02 -0400

    commit tree a15500ff0c2e9f993dd1524f7ae1f716860dc199

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+ let originialWord = []
+ let reverseWord = []
+ for (letter of word){
+    originialWord.unshift(letter)
+    reverseWord.push(letter)
+ }
+
+ let a = originialWord.join()
+ let b = reverseWord.join()
+ 
+ return a===b
 }
 
 /* 
   Add your pseudocode here
+
+
 */
 
 /*
   Add written explanation of your solution here
+  Create two empty arrays to store letters of word and its reverse.
+  Iterate through letters of the word while adding them to the respective arrays using unshift and push
+  c
 */
 
 // You can run `node index.js` to view these console logs
